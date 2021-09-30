680ad53f5b8ff75e612e9f7e5878d761b4a7c8a3
Phoenix-Cinder <phoenixcinder@me.com>
2021-09-30 18:04:37 +1000

    completed

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution in this file!
+const employee = {
+    name: 'Bree',
+    streetAddress: 'Windsor Street'
+  };
+  
+  function updateEmployeeWithKeyAndValue(obj,key,value)
+  {
+    const newObj = { ...employee};
+    newObj[key]=value;
+    return newObj;
+  }
+  const newEmployee = updateEmployeeWithKeyAndValue(employee,'name','Gary')
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(obj,key,value)
+  {
+    obj[key] = value;
+    return obj;
+  }
+  const changeEmployee=destructivelyUpdateEmployeeWithKeyAndValue(employee,'name','John')
+  
+  function deleteFromEmployeeByKey(obj,key,value)
+  {
+   const newObject = { ...employee};
+    newObject[key]=value;
+    return newObject;
+  }
+  const newEmp = deleteFromEmployeeByKey(employee,'name','Kruti')
+   
+  function destructivelyDeleteFromEmployeeByKey(obj,key,value)
+  {
+    obj[key] = value;
+    return obj;
+  }
+  const changeEmp=destructivelyUpdateEmployeeWithKeyAndValue(employee,'name','Alice')
+  
